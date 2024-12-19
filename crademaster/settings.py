b148e266e52f3e2c5c42b874984ad4a3705eef5d
kapphire <nodejs1987@gmail.com>
2024-12-19 22:37:20 +0900

    db conf updated

--- crademaster/settings.py
+++ crademaster/settings.py
@@ -110,9 +110,9 @@ WSGI_APPLICATION = 'crademaster.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': 'crademaster',
+        'USER': 'crademaster',
+        'PASSWORD': 'crademaster',
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     }
