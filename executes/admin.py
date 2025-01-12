354237c028083ff980543916487e32ad6a8b3f34
kapphire <nodejs1987@gmail.com>
2025-01-13 07:07:50 +0900

    executes admin updated

--- executes/admin.py
+++ executes/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from .models import Execute
 
 class ExecuteAdmin(admin.ModelAdmin):
-    list_display = ['user', 'amount', 'duration', 'created']
+    list_display = ['user', 'amount', 'duration', 'profit_percent', 'created']
     list_filter = ['user', 'duration']
     search_fields = ['user__username', 'amount']
-    fields = ['user', 'amount', 'duration', 'created']
+    fields = ['user', 'amount', 'duration', 'profit_percent', 'created']
 
 
 admin.site.register(Execute, ExecuteAdmin)
