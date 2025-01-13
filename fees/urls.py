1635e4becc9417fc3a6064a499e9555a317c631a
kapphire <nodejs1987@gmail.com>
2025-01-13 09:00:13 +0900

    royalty fee updates

--- fees/urls.py
+++ fees/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import FeeListView, FeeUpdateView
+from .views import (
+    FeeListView, FeeUpdateView, RoayltyFeeListView, RoyaltyFeeUpdateView
+)
 
 urlpatterns = [
     path('', FeeListView.as_view(), name='fee_list'),
     path('<int:pk>/edit/', FeeUpdateView.as_view(), name='fee_update'),
+    path('royalty/', RoayltyFeeListView.as_view(), name='royalty_fee_list'),
+    path('royalty/<int:pk>/edit/', RoyaltyFeeUpdateView.as_view(), name='royalty_fee_update'),
 ]
--- fees/views.py
+++ fees/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView
 
 from authentication.mixins import StaffRequiredMixin
-from .models import Fee
+from .models import Fee, RoyaltyFee
 
 class FeeListView(StaffRequiredMixin, ListView):
     model = Fee
@@ -19,3 +19,20 @@ class FeeUpdateView(StaffRequiredMixin, UpdateView):
 
     def get_object(self):
         return Fee.objects.get(id=self.kwargs['pk'])
+
+
+class RoayltyFeeListView(StaffRequiredMixin, ListView):
+    model = RoyaltyFee
+    template_name = 'fees/royalty_list.html'
+    context_object_name = 'fees'
+
+
+class RoyaltyFeeUpdateView(StaffRequiredMixin, UpdateView):
+    model = RoyaltyFee
+    fields = ['min_investment', 'max_investment', 'fee_percentage']
+    template_name = 'fees/royalty_update.html'
+    context_object_name = 'fee'
+    success_url = reverse_lazy('royalty_fee_list')
+
+    def get_object(self):
+        return RoyaltyFee.objects.get(id=self.kwargs['pk'])
